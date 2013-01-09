b441b6d2888b36078b77072bba24358f728eebe2
Tanner Mckenney <tmckenney7@gmail.com>
2013-01-08 23:49:06 -0500

    removed file backups.

---
+++ leveldata/multiplayer/advanced-deathmatch.lua
@@ -0,0 +1,289 @@
+GUID = { 110, 91, 157, 190, 18, 23, 250, 78, 144, 20, 41, 246, 181, 128, 214, 12, }
+GameRulesName = "Adv. Deathmatch"
+Description = "Advanced Game Options"
+
+dofilepath("data:soundscripts/music.lua")
+
+GameSetupOptions = {
+
+	{
+		name = "resources",
+		locName = "$3240",
+		tooltip = "$3239",
+		default = 1,
+		visible = 1,
+		choices = { "$3241", "2.0", "$3242", "2.5", "$3243", "3.0", }, 
+	},
+	
+	{
+		name = "unitcaps",
+		locName = "$3214",
+		tooltip = "$3234",
+		default = 1,
+		visible = 1,
+		choices = { "$3215", "Small", "$3216", "Normal", "$3217", "Large"}, 
+	},
+	
+	{
+		name = "resstart",
+		locName = "$3205",
+		tooltip = "$3232",
+		default = 0,
+		visible = 1,
+		choices = { "$3206", "1000", "$3207", "3000", "$3208", "10000", "$3209", "0", }, 
+	},
+	
+	{
+		name = "lockteams",
+		locName = "$3220",
+		tooltip = "$3235",
+		default = 0,
+		visible = 1,
+		choices = { "$3221", "yes", "$3222", "no", }, 
+	},
+	
+	{
+		name = "startlocation",
+		locName = "$3225",
+		tooltip = "$3237",
+		default = 1,
+		visible = 1,
+		choices = { "$3226", "random", "$3227", "fixed", }, 
+	},
+	
+	{
+		name = "startfleet",
+		locName = "Starting Mode",
+		tooltip = "Choose A Starting Fleet",
+		default = 0,
+		visible = 1,
+		choices =
+		{
+			"Factory", "1",
+			"Carrier", "2",
+			"Fleet", "3",
+		},
+	},
+	
+	{
+		name = "wincondition",
+		locName = "Win Condition",
+		tooltip = "select the condition for winning the game",
+		default = 0,
+		visible = 1,
+		choices =
+		{ 
+			"Default", 0, 
+			"Destroy Team Production", 1, 
+			"Destroy All ships", 2, 
+			"Quit Manually", 3, 
+		},
+	},
+	
+	{
+		name = "randommusic",
+		locName = "Music",
+		tooltip = "Select the background music",
+		default = 0,
+		visible = 1,
+		choices = musicChoices,
+	},
+}
+ 
+dofilepath("data:scripts/scar/restrict.lua")
+
+Events = {}
+Events.endGame =
+    {
+        {
+            { "wID = Wait_Start(5)", "Wait_End(wID)", },
+        },
+    }
+
+function OnInit()
+
+    MPRestrict()
+
+	Play(GetGameSettingAsString("randommusic"))
+
+	wincondition = GetGameSettingAsNumber("wincondition")
+
+	if wincondition == 0 then
+		Rule_AddInterval("CheckPlayerProductionShipsLeftRule", 1)
+	elseif wincondition == 1 then
+		Rule_AddInterval("CheckTeamProductionShipsLeftRule", 1)
+	elseif wincondition == 2 then
+		Rule_AddInterval("CheckTeamAnyShipsLeftRule", 1)
+	elseif wincondition == 3 then
+		Rule_AddInterval("DoNotQuit", 1)
+	else
+		Rule_AddInterval("CheckPlayerProductionShipsLeftRule", 1)
+	end
+
+
+    Rule_Add("MainRule")
+
+	startfleet = GetGameSettingAsNumber("startfleet")
+
+	if startfleet == 1 then
+		SetStartFleetSuffix("")
+	elseif startfleet == 2 then
+		SetStartFleetSuffix("_light")
+	elseif startfleet == 3 then
+		SetStartFleetSuffix("_heavy")
+	else
+		SetStartFleetSuffix("")
+	end
+	
+end
+
+
+AnyPlayerIndex = 0
+
+-- Kills a player if the player has no production capability
+
+function CheckPlayerProductionShipsLeftRule()
+	if ((Player_IsAlive(AnyPlayerIndex) == 1) and (Player_HasShipWithBuildQueue(AnyPlayerIndex) == 0)) then
+		Player_Kill(AnyPlayerIndex)
+	end
+	if (AnyPlayerIndex == (Universe_PlayerCount() - 1)) then
+		AnyPlayerIndex = 0
+	else
+		AnyPlayerIndex = AnyPlayerIndex + 1
+	end
+end
+
+-- Kills a player if no team member has any production capability
+
+function CheckTeamProductionShipsLeftRule()
+	local bDead = 1
+	for otherPlayerIndex = 0, (Universe_PlayerCount() - 1) do
+		if ((AreAllied(AnyPlayerIndex, otherPlayerIndex) == 1) and (Player_IsAlive(otherPlayerIndex) == 1) and (Player_HasShipWithBuildQueue(otherPlayerIndex) == 1)) then
+			bDead = 0
+			break
+		end
+	end
+	if (bDead == 1) then
+		Player_Kill(AnyPlayerIndex)
+	end
+	if (AnyPlayerIndex == (Universe_PlayerCount() - 1)) then
+		AnyPlayerIndex = 0
+	else
+		AnyPlayerIndex = AnyPlayerIndex + 1
+	end
+end
+
+-------------------------------------------------------------------------------
+-- Kills a player if no team member has any ships
+--
+function CheckTeamAnyShipsLeftRule()
+	local bDead = 1
+	for otherPlayerIndex = 0, (Universe_PlayerCount() - 1) do
+		if ((AreAllied(AnyPlayerIndex, otherPlayerIndex) == 1) and (Player_IsAlive(otherPlayerIndex) == 1) and (Player_NumberOfShips(otherPlayerIndex) > 0)) then
+			bDead = 0
+			break
+		end
+	end
+	if (bDead == 1) then
+		Player_Kill(AnyPlayerIndex)
+	end
+	if (AnyPlayerIndex == (Universe_PlayerCount() - 1)) then
+		AnyPlayerIndex = 0
+	else
+		AnyPlayerIndex = AnyPlayerIndex + 1
+	end
+end
+
+
+-------------------------------------------------------------------------------
+-- Stops the game from terminating even when all enemies are gone
+--
+function DoNotQuit()
+end
+
+-------------------------------------------------------------------------------
+-- counts the size of a player's fleet
+--
+function Player_NumberOfShips(iPlayer)
+	local iRace = Player_GetRace(iPlayer) + 1
+	local ShipCount = 0
+	dofilepath([[data:scripts/race.lua]])
+	dofilepath([[data:scripts/building and research/]] .. races[iRace][1] .. [[/build.lua]])
+	for i, iCount in build do
+		if (iCount.Type ~= 1) then
+			ShipCount = ShipCount + Player_GetNumberOfSquadronsOfTypeAwakeOrSleeping(iPlayer, iCount.ThingToBuild)
+		end
+	end
+	return ShipCount
+end
+
+-------------------------------------------------------------------------------
+-- returns the team number of the player (may be different than in the game-setup screen)
+--
+function Player_Team(iPlayer)
+	local TeamsTable = {}
+	for playerIndex = 0, (Universe_PlayerCount() - 1) do
+		local IsAllied = 0
+		for i = 1, getn(TeamsTable) do
+			if (AreAllied(playerIndex, TeamsTable[i]) == 1) then
+				IsAllied = 1
+				break
+			end
+		end
+		if (IsAllied == 0) then
+			tinsert(TeamsTable, playerIndex)
+		end
+	end
+	for i = 1, getn(TeamsTable) do
+		if (AreAllied(iPlayer, TeamsTable[i]) == 1) then
+			return i
+		end
+	end
+end
+
+
+function MainRule()
+	local numAlive = 0
+	local numEnemies = 0
+	local gameOver = 1
+	-- check to see if ALL of our enemies are dead, then gameOver
+	for playerIndex = 0, (Universe_PlayerCount() - 1) do
+		-- only process 'alive' players
+		if (Player_IsAlive(playerIndex) == 1) then
+			-- compare this player against all others
+			for otherPlayerIndex = 0, (Universe_PlayerCount() - 1) do
+				-- are enemies?
+				if (AreAllied(playerIndex, otherPlayerIndex) == 0) then
+					-- is the enemy alive - if so the game is still on
+					if (Player_IsAlive(otherPlayerIndex) == 1) then
+						gameOver = 0
+					else
+						numEnemies = numEnemies + 1
+					end
+				end
+			end
+			numAlive = numAlive + 1
+		end
+	end
+	-- special case - if there are no enemies then never end
+	if ((numEnemies == 0) and (numAlive > 0)) then
+		gameOver = 0
+	end
+	-- if gameOver flag is still set then the game is OVER
+	if (gameOver == 1) then
+		if (GetGameSettingAsNumber("enablestats") == 1) then
+			WriteStats()
+		end
+		Rule_Add("waitForEnd")
+		Event_Start("endGame")
+		Rule_Remove("MainRule")
+	end
+end
+
+function waitForEnd()
+	if (Event_IsDone("endGame")) then
+		setGameOver()
+		Rule_Remove("waitForEnd")
+	end
+end
+
---
+++ soundscripts/music.lua
@@ -0,0 +1,138 @@
+playedBin = {}
+
+--Call this variable into your game type choices. 
+
+musicChoices = {
+			"Off", "mute",
+			"Shuffle: All", "shuffle",
+			"Shuffle: Ambient", "ambient",
+			"Shuffle: Staging", "staging",
+			"Shuffle: Battle", "battle",
+			"Ambient: No.1", "ambient\\amb_01",
+			"Ambient: No.2", "ambient\\amb_02",
+			"Ambient: No.3", "ambient\\amb_03",
+			"Ambient: No.4", "ambient\\amb_04",
+			"Ambient: No.5", "ambient\\amb_05",
+			"Ambient: No.6", "ambient\\amb_06",
+			"Ambient: No.7", "ambient\\amb_07",
+			"Ambient: No.8", "ambient\\amb_08",
+			"Ambient: No.9", "ambient\\amb_12",
+			"Ambient: No.10", "ambient\\amb_13",
+			"Ambient: No.11", "ambient\\amb_14",
+			"Battle: No.2", "battle\\battle_04",
+			"Battle: No.3, Alternate", "battle\\battle_04_alt",
+			"Battle: No.4", "battle\\battle_06",
+			"Battle: Arrival", "battle\\battle_arrival",
+			"Battle: Keeper", "battle\\battle_keeper",
+			"Battle: Movers", "battle\\battle_movers",
+			"Battle: Planet Killers", "battle\\battle_planetkillers",
+			"Battle: Sajuuk", "battle\\battle_sajuuk",
+			"Staging: No.1", "staging\\staging_01",
+			"Staging: No.2", "staging\\staging_04",
+			"Staging: No.3", "staging\\staging_05",
+			"Staging: No.4", "staging\\staging_08",
+			"Staging: No.5", "staging\\staging_11",
+}
+
+function Play(Music)
+
+	if Music == nil then
+		ShuffleAll()
+				
+	elseif Music == "shuffle" then
+		ShuffleAll()
+		
+	elseif Music == "ambient" then
+		ShuffleAmbient()
+		
+	elseif Music == "battle" then
+		ShuffleBattle()
+		
+	elseif Music == "staging" then
+		ShuffleStaging()
+			
+	elseif Music == "mute" then
+		Sound_MusicPlay("data:sound\\music\\staging\\Mute" )
+		
+	elseif Music == "credits" then
+		Sound_MusicPlay("data:sound\\music\\staging\\Credits" )	
+		
+	else
+		Sound_MusicPlay("data:sound\\music\\" .. Music)
+	end
+	
+end
+
+function ShuffleAll()
+
+	UI_BindKeyEvent(F1KEY, "RandomMusicRule")
+	dofilepath("data:soundscripts/playlists/allmusic.lua")
+	Rule_Add("RandomMusicRule")
+end
+
+function ShuffleAmbient()
+	
+	UI_BindKeyEvent(F1KEY, "RandomMusicRule")
+	dofilepath("data:soundscripts/playlists/ambientonly.lua")
+	Rule_Add("RandomMusicRule")
+	
+end
+
+function ShuffleBattle()
+	
+	UI_BindKeyEvent(F1KEY, "RandomMusicRule")
+	dofilepath("data:soundscripts/playlists/battleonly.lua")
+	Rule_Add("RandomMusicRule")
+	
+end
+
+function ShuffleStaging()
+	
+	UI_BindKeyEvent(F1KEY, "RandomMusicRule")
+	dofilepath("data:soundscripts/playlists/stagingonly.lua")
+	Rule_Add("RandomMusicRule")
+	
+end
+    
+--Keybinded functions.
+function RandomMusicRule()
+	RandomMusic(PlayList)
+end
+	
+
+function RandomMusic(tPlaylist)
+	-- function created by Mikail, EvilleJedi
+	-- Input:	<tPlaylist>: the playlist (a table) of songs.
+	local passBool = 1
+	local musicPath = "data:sound\\music\\"
+	local listLen = getn(tPlaylist)
+	local binLen = getn(playedBin)
+	local randNum = random(listLen)
+	local track_file = musicPath .. tPlaylist[randNum][1]
+	local track_title = tPlaylist[randNum][2]
+	local track_length = tPlaylist[randNum][3]
+	local track_m = floor(track_length / 60)
+	local track_s = track_length - track_m * 60
+	local track_string = "Now playing (" .. randNum .. "/" .. listLen .. "): " .. track_title .. " (" .. track_m .. "m " .. track_s .. "s)"
+	for k = 1, binLen do
+		-- don't play the same track twice
+		if (playedBin[k] == randNum) then
+			passBool = 0
+			-- if the end of the list has been reached, start over
+			if (k == listLen) then
+				playedBin = {}
+			end
+			break
+		end
+	end
+	if (passBool == 0) then
+		RandomMusic(tPlaylist)
+	else
+		Sound_MusicPlay(track_file)
+		Subtitle_Message(track_string, 10)
+		print(track_string)
+		tinsert(playedBin, randNum)
+		Rule_AddInterval("RandomMusicRule", track_length)
+		Rule_Remove("RandomMusicRule")
+	end
+end
